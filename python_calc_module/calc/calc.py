d435cbdaddf6ef7563ce55cde3913dcab34d08fd
Yugant <hadiyalyugant@gmail.com>
2020-06-07 17:43:52 +0200

    reshaping repo

---
+++ python_calc_module/calc/calc.py
@@ -0,0 +1,9 @@
+import sys
+   
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        add(int(sys.argv[2]),int(sys.argv[3]))
+    else:
+        print("ERROR:",str(len(sys.argv)-1)," arguments given instead of 3")
