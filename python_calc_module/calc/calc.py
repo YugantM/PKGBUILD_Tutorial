623dcd2801d6ab7c6c022cb389cd71a945aff503
Yugant M. Hadiyal <hadiyalyugant@gmail.com>
2020-06-12 13:06:37 +0200

    Update calc.py
    changes in calc

--- python_calc_module/calc/calc.py
+++ python_calc_module/calc/calc.py
@@ -1,9 +1,13 @@
 import sys
 import add
 
+def calc(o,a,b):
+   if o==0:
+      print(add(a,b))
+      return add(a,b)
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
-        add.add(int(sys.argv[2]),int(sys.argv[3]))
+        return calc(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
     else:
         print("ERROR:",str(len(sys.argv)-1)," arguments given instead of 3")
