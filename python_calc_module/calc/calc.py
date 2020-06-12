9c907a262866490187f75ac408b6adab123380b4
Yugant Hadiyal <hadiyalyugant@gmail.com>
2020-06-12 13:35:36 +0200

    calc.py

--- python_calc_module/calc/calc.py
+++ python_calc_module/calc/calc.py
@@ -12,6 +12,6 @@ def calc(o,a,b):
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
-        return calc(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
+        calc(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
     else:
         print("ERROR:",str(len(sys.argv)-1)," arguments given instead of 3")
