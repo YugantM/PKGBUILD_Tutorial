9058eec73f9491de218e1515e62c3fccff0d3150
Yugant Hadiyal <hadiyalyugant@gmail.com>
2020-06-28 13:09:00 +0200

    depends.py

--- package_graph_visualization/depends.py
+++ package_graph_visualization/depends.py
@@ -37,7 +37,7 @@ def sync():
 	# Checks if file is present in the current directory or not
 	if file_found:
 
-		# generates list of package which are changed [added or removed]
+		# generates list of package which occurs change in the system's package-list [added or removed]
 		changes_in_package_list = list(set(pd.read_csv(file_name).index) & set([each.split(" ")[0] for each in lcom("pacman -Q") if each!=""]) )
 
 
@@ -63,50 +63,65 @@ def sync():
 		# generating list of packages
 		cols = [each.split("\n")[0] for each in module_list if each.split("\n")[0] != None]
 
-
+		# creates a blank dataframe where index and columns represents all the packages
+		# shape of the frame is NxN, N = number of packages available in the system
 		dependency_df = pd.DataFrame(index=cols,columns=cols)
 
+		# fills the dataframe with 0
 		dependency_df = dependency_df.fillna(0)
 
-
+		# this loop interates through all the columns
 		for each_module in list(dependency_df.columns):
 
+			# this will print name of each module
 			print("#")
 			print(each_module)
 			
+			# this is a temporary list which gets dependencies for the given package
 			temp_list = lcom("""pacman -Qi """+str(each_module)+"""|grep "Depends On"|cut -d: -f2""")
 
+			# cleaning the list
 			temp_list = temp_list[0].split(" ")[1:]
 			
+			# this is the list which will hold names of the packages as dependecies for the given package[each_module]
 			dependency_list = []
 
+			# this loop iterates through the temporary list 
 			for each in temp_list:
-
 				
+				# this loop iterates through list of all the packages
 				for each_temp_module in module_list:
 					
+					# checks and appends the names of the dependecies
 					temp = sub_check(each_temp_module,each)
 					if temp!=None:
 						dependency_list.append(temp)
 
+			# prints the list of dependecies
 			print(dependency_list)
 
+			# this loop will fill the row with 1 if a package has the dependency
 			for each_dependecy in dependency_list:
 
 				dependency_df.loc[each_module,each_dependecy] = 1
 
+		# writes the csv file from dataframe
 		dependency_df.to_csv(file_name)
 
+		# if update has found in the system prints and returns dataframe
 		if update_found:
 			print("Adjancency matrix")
 			return dependency_df
 	
+	# if no updates in the system, reads the csv file and returns the dataframe
 	print("Adjancency matrix")	
 	return pd.read_csv(file_name).set_index('Unnamed: 0')
-		# --------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------
 
 
 
+# below code is for generating graph
+# will write proper comments after finalizing the code
 print('printing G2')
 
 G2 = nx.from_pandas_adjacency(sync())
